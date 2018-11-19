cf11fa433a0cca3913198af85bf363359a023d7d
yywingliang <45158402+yywingliang@users.noreply.github.com>
2018-11-19 12:50:55 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,11 +6,11 @@ using Test
 #Test with analytical derivates
 f(x)=x^2
 f′(x)=2x
-@test newtonroot(f,f′,x₀=0.5).root ≈ 0.0
+@test newtonroot(f,f′,x₀=0.5).root ≈ 0.0 atol=0.0000001
 
 f(x)=x^3-3x^2+2
 f′(x)=3x^2-6x
-@test newtonroot(f,f′,x₀=0.5).root ≈ 1.0
+@test newtonroot(f,f′,x₀=0.5).root ≈ 1.0 atol=0.0000001
 
 f(x)=log(x)-1
 f′(x)=1/x
@@ -19,24 +19,24 @@ f′(x)=1/x
 
 #Test with automatic derivatives
 f(x)=x^2
-@test newtonroot(f,x₀=0.5).root ≈ 0.0
+@test newtonroot(f,x₀=0.5).root ≈ 0.0 atol=0.0000001
 
 f(x)=x^3-3x^2+2
-@test newtonroot(f,x₀=0.5).root ≈ 1.0
+@test newtonroot(f,x₀=0.5).root ≈ 1.0 atol=0.0000001
 
 f(x)=log(x)-1
 @test newtonroot(f,x₀=0.5).root ≈ 2.71828182845904523536
 
 
 #Test Big Float
-f(x)=sin(x)-1
+ff(x)=sin(x)-1
 f′(x)=cos(x)
-@test newtonroot(f, f′, x₀ = BigFloat(1.0), tolerance = 1E-32).root ≈ BigFloat(pi/2)
-@test newtonroot(f, x₀ = BigFloat(1.0), tolerance = 1E-32).root ≈ BigFloat(pi/2)
+@test newtonroot(f, f′, x₀ = BigFloat(1.0), tolerance = 1E-32).root ≈ BigFloat(pi/2) atol = 1E-16
+@test newtonroot(f, x₀ = BigFloat(1.0), tolerance = 1E-32).root ≈ BigFloat(pi/2) atol = 1E-16
 
 
 #Test non-convergence
-f(x)=2+x^2
+f(x)=x^2+2
 @test newtonroot(f,x₀=0.5).root == nothing
 
 
@@ -47,11 +47,11 @@ f(x)=log(x)-1
 
 #Test tolerance
 f(x)=log(x)-1
-@test newtonroot(f,x₀=0.5).root ≈ 1.0
+@test newtonroot(f,x₀=0.5).root ≈ 2.71828182845904523536
 r1=newtonroot(f,x₀=1.0).root
 r2=newtonroot(f,x₀=1.0, tolerance=0.00001).root
 r3=newtonroot(f,x₀=1.0, tolerance=0.001).root
-@test norm(f(r1)) < norm(f(r2)) < norm(f(r3)) # accuracy decreases as tolerance increases
+@test norm(f(r1)) <= norm(f(r2)) <= norm(f(r3)) # accuracy decreases as tolerance increases
 
 
 end
