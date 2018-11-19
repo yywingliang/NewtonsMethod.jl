e5795bc537f026d534b2c3ddfe94847aab71c168
yywingliang <45158402+yywingliang@users.noreply.github.com>
2018-11-19 10:36:13 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,14 @@ using NewtonsMethod
 using Test
 
 @testset "NewtonsMethod.jl" begin
-    # Write your own tests here.
+
+#Test 1
+
+f1(x) = (x-1.0)^3
+ f1′(x) = 3.0*(x-1.0)^2
+ x1₀ = 0.1
+ @test newtonroot(f1, f1′, x₀ = x1₀).root ≈ 0.9999998168636032
+ @test newtonroot(f1,      x₀ = x1₀).root ≈ 0.9999998168636032
+
+
 end
